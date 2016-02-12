79d243ee410ef8af24152b409185870a138c5ff2
zhenyaracheva <zhenya.racheva@gmail.com>
2016-02-12 02:40:58 -0800

    Cleared Login.cshtml

--- BulBike.Web/Models/AccountViewModels/LoginViewModel.cs
+++ BulBike.Web/Models/AccountViewModels/LoginViewModel.cs
@@ -6,11 +6,13 @@
     {
         [Required]
         [Display(Name = "Username")]
+        [UIHint("SingleLineText")]
         public string Username { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
+        [UIHint("Password")]
         public string Password { get; set; }
 
         [Display(Name = "Remember me?")]
